83f5a9ca80f27a41a6c444a3a07d1f90fa68ced4
Fatih Gören <fatihgoren61@gmail.com>
2022-03-24 21:20:38 +0300

    Add project files.

---
+++ Data/MyContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SqLiteApp.Models;
+
+namespace SqLiteApp.Data
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=C:\SqLiteDb\User.db");
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SqLiteApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
